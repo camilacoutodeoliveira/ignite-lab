280341fe0a96f98eb39a0ff4ad0adef8d95f3c0d
Camila Couto <camilacoutodeoliveira@gmail.com>
2022-06-21 22:05:33 -0300

    set uri

--- event-platform/src/lib/apollo.ts
+++ event-platform/src/lib/apollo.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
-    uri: '',
+    uri: 'https://api-sa-east-1.graphcms.com/v2/cl4obko4k1qqf01xxd1x4a8vu/master',
     cache: new InMemoryCache()
 })
